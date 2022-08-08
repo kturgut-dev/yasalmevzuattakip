df56b43aa502aa481c2b9f7112ed3ebb63380889
Kürşat Turgut <kursat.turgut@ntm.com.tr>
2022-08-08 11:49:54 +0300

    first commit

---
+++ KYSJob.cs
@@ -0,0 +1,20 @@
+﻿using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KYS.YasalMevzuatTakip
+{
+    public class KYSJob : IJob
+    {
+        public Task Execute(IJobExecutionContext context)
+        {
+            return Task.Run(() =>
+            {
+                new JobFuncs().CheckNewspaperToDay();
+            });
+        }
+    }
+}
---
+++ Models/ProjectSetting.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KYS.YasalMevzuatTakip.Models
+{
+    public class ProjectSetting
+    {
+        public MailConf MailConf { get; set; }
+        public MailRecipient MailRecipients { get; set; }
+        public string[] SearchKeys { get; set; }
+        public bool LoggingIsOn { get; set; }
+    }
+}
